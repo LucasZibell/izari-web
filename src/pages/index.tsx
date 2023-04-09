a7b3bd2433e247c3916068bbf05f2605aa0ea953
Lucas Zibell <lucaszibell@MacBook-Pro.local>
2023-04-09 19:30:46 -0300

    added eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import {Typography, Button} from '@mui/material';
-import { Inter } from 'next/font/google'
+import { Typography, Button } from '@mui/material'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function Home() {
   return (
     <>
       <Head>
@@ -22,3 +18,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home
